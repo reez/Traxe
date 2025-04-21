dc1ff9e2cdc301c69eeb1cb11b5a75586efb86bc
Matthew <ramsden@hey.com>
2025-04-21 14:06:33 -0500

    add logger

---
+++ Traxe/Utilities/Logger+Extensions.swift
@@ -0,0 +1,17 @@
+import os.log
+
+extension Logger {
+    private static var subsystem = Bundle.main.bundleIdentifier!
+
+    /// Logs network-related activities.
+    static let networking = Logger(subsystem: subsystem, category: "networking")
+
+    /// Logs settings-related activities.
+    static let settings = Logger(subsystem: subsystem, category: "settings")
+
+    /// Logs dashboard-related activities.
+    static let dashboard = Logger(subsystem: subsystem, category: "dashboard")
+
+    /// Logs onboarding-related activities.
+    static let onboarding = Logger(subsystem: subsystem, category: "onboarding")
+} 
